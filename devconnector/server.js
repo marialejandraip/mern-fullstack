d283079db9c725b626dc3f95ec3471d10e6d0675
marialejandraip <mariaibanez07@gmail.com>
2021-06-07 18:56:38 -0500

    created routes files on api

--- devconnector/server.js
+++ devconnector/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const connectdb = require('./config/db');
 
 const app = express();
 
+//Connect db
+connectdb();
+
 app.get('/', (req, res)=> res.send('<h1>Hola mundo</h1>'))
 
-const PORT = process.env.PORT || 5000;
+//Define routes
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, ()=> console.log(`Server started on ${PORT}`));
